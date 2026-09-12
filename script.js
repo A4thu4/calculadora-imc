cd827e07dc5270eee5e7a7eddc9ad1185205b359
A4thu4 <arthurmamedesborges@gmail.com>
2026-09-12 08:26:15 -0300

    feat(imc): ajusta textos de classificacao do IMC

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function calcularIMC(peso, altura) {
 }
 
 function classificarIMC(imc) {
-  if (imc < 18.5) return 'Abaixo do peso';
+  if (imc < 18.5) return 'Abaixo do pesoo';
   if (imc < 25) return 'Peso normal';
-  if (imc < 30) return 'Sobrepeso';
+  if (imc < 30) return 'Sobrepesoo';
   if (imc < 35) return 'Obesidade grau I';
   if (imc < 40) return 'Obesidade grau II';
   return 'Obesidade grau III';
